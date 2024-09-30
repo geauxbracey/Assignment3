0a9479dee3b53bfb3d86093f6ae5f206f36b55ab
Kaitlyn <braceykaitlyn@gmail.com>
2024-09-30 14:31:19 -0400

    Initial Commit

---
+++ Assignment3/Assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3App.swift
+//  Assignment3
+//
+//  Created by Kaitlyn Bracey on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment3Tests/Assignment3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3Tests.swift
+//  Assignment3Tests
+//
+//  Created by Kaitlyn Bracey on 9/30/24.
+//
+
+import Testing
+@testable import Assignment3
+
+struct Assignment3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
